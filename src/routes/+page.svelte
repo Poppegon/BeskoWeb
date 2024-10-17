863bef3b9230444ca90f0a977552e2ba468aecbd
Poppegon <alvinboy0705@gmail.com>
2024-10-17 13:40:32 +0200

    CommitName

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking cocaine! aw dwaaaa</h1>
-		<p>Start by exploring:</p>
+		<h1 class="h1">Let's get cracking bones!</h1>
+		<p>Start by asking Samir for help</p>
 		<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
 			<li><code class="code">/src/app.postcss</code> - app wide css</li>
